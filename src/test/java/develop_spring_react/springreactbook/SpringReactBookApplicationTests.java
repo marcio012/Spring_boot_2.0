4d108c25560a960bb6d9f7cbf98a73cf74ecd79e
marcio heleno <marcio012@edu.unifor.br>
2018-10-30 20:32:33 -0300

    teste unitarios jUnit

--- src/test/java/develop_spring_react/springreactbook/SpringReactBookApplicationTests.java
+++ src/test/java/develop_spring_react/springreactbook/SpringReactBookApplicationTests.java
@@ -1,16 +1,24 @@
 package develop_spring_react.springreactbook;
 
+import develop_spring_react.springreactbook.web.CarController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringReactBookApplicationTests {
 
+    @Autowired
+    private CarController controller;
+
     @Test
     public void contextLoads() {
+        assertThat(controller).isNotNull();
     }
 
 }
